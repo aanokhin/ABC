695927efff3931be1606e63d5450f42050e6617a
Andre Anokhin <aanokhin@microbalt.com>
2019-06-03 16:42:31 -0500

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 (function () {
   'use strict';
   const gulp = require('gulp');
-  const plugins = require('gulp-load-plugins')();
 
   gulp.task('build', function () {
     console.log('building-------------');
